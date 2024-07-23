8c5da7a0d0ef278d050b2370b07f1988f83602ef
taniajose95 <84635914+taniajose95@users.noreply.github.com>
2024-07-23 01:33:54 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = var.region
 }
 
-resource "aws_instance" "example" {
+resource "aws_instance" "aws_instance" {
   ami           = var.ami_id
   instance_type = var.instance_type
 
